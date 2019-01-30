eadb1ca6790a93264ab97facbc7e7fac548a5e2c
alejandroqm23 <root@debianDSW.Debiandsw.org>
2019-01-30 19:06:51 +0000

    Añadido salto de linea 2

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var upper = require('upper-case');
 const PORT = process.env.PORT || 5000;
 http.createServer(function (req, res){
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('Hola desde Heroku \n');
+        res.write('Hola desde Heroku ');
+	res.write('<br>');
 	res.write('Prueba de Deploy automatico');
         res.end()
 }).listen(PORT);
