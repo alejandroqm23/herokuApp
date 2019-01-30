3d82db5689bdc34eacab10b8b5f5a970fb102d8d
alejandroqm23 <43794737+alejandroqm23@users.noreply.github.com>
2019-01-30 18:19:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var http = require ('http');
 var upper = require('upper-case');
+const PORT = process.env.PORT || 5000;
 http.createServer(function (req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write(uc('Hola mundito mundial!!!!!!!'));
-	res.end()
-}).listen(8080);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(uc('Hola mundito mundial!!!!!!!'));
+        res.end()
+}).listen(PORT);
