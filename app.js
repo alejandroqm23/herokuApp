5b53e37d2cb00c624fd251e0da4ea83387464b04
alejandroqm23 <root@debianDSW.Debiandsw.org>
2019-01-30 18:42:10 +0000

    Cuarto intento

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var upper = require('upper-case');
 const PORT = process.env.PORT || 5000;
 http.createServer(function (req, res){
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(uc('Hola mundito mundial!!!!!!!'));
+        res.write('Hola desde Heroku');
         res.end()
 }).listen(PORT);
